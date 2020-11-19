89277899e544fdb9732d938b22efaebf7cdd3a5a
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-11-18 23:24:38 -0800

    Add files via upload

--- assign4.c
+++ assign4.c
@@ -243,7 +243,30 @@ char* get_buff_2(){
 
 
 
-
+void removeDuplicates(char* S) 
+{ 
+    // When string is empty, return 
+    if (S[0] == '\0') 
+        return; 
+  
+    // if the adjacent characters are same 
+    if (S[0] == S[1]) { 
+          
+        // Shift character by one to left 
+        int i = 0;  
+        while (S[i] != '\0') { 
+            S[i] = S[i + 1]; 
+            i++; 
+        } 
+  
+        // Check on Updated String S 
+        removeDuplicates(S); 
+    } 
+  
+    // If the adjacent characters are not same 
+    // Check from S+1 string address 
+    removeDuplicates(S + 1); 
+} 
 
 void *changePlusSign(void *args)
 {
@@ -286,30 +309,7 @@ void *changePlusSign(void *args)
 		}
 				//	line[x+1] = line[x+2];
 				
-void removeDuplicates(char* S) 
-{ 
-    // When string is empty, return 
-    if (S[0] == '\0') 
-        return; 
-  
-    // if the adjacent characters are same 
-    if (S[0] == S[1]) { 
-          
-        // Shift character by one to left 
-        int i = 0;  
-        while (S[i] != '\0') { 
-            S[i] = S[i + 1]; 
-            i++; 
-        } 
-  
-        // Check on Updated String S 
-        removeDuplicates(S); 
-    } 
-  
-    // If the adjacent characters are not same 
-    // Check from S+1 string address 
-    removeDuplicates(S + 1); 
-} 
+
 //  	
 				
 				//printf("%c\n", line[x]);
