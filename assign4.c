796aac46f633e9ffb02b4874a198b20d78bb5fbd
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2021-03-26 03:40:45 -0700

    Add files via upload

--- assign4.c
+++ assign4.c
@@ -401,7 +401,7 @@ void *write_output(void *args)
     int i=0;
     
     char temp[NUM_ITEMS];
-    int length = 0;
+    size_t length = 0;
     int size3 = 0;
     
     
@@ -418,7 +418,7 @@ void *write_output(void *args)
 		
 		length = sizeof(temp) / sizeof(temp[0]);
 		
-		printf("size is %d \n", length);
+		printf("size is %z:", length);
     
 	
 ////////////////////////////////////////////////////////gts length of array	
